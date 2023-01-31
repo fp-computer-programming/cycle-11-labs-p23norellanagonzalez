5f91aeca48840460b4b3ad3707064fb9fb88e61d
Nickolas. Orellana <p23norellanagonzalez@fairfieldprep.org>
2023-01-31 10:53:56 -0500

    lab 1 done
    starting lab 2

---
+++ Lab_11-1.py
@@ -0,0 +1,11 @@
+# author NDO 01/31/23 
+
+#created a dictionary called "row"
+#added key value pairs according to the row set up from first semester 
+# Printed the dictionary and the items listed in it
+row = { 11: 'Nickolas Orellana',  12 : 'Ethan Forest' , 13 : 'Robbie Donahue', 14 : 'Jack Rydzik' }
+print(row)
+
+# Create a scenario where the dictionary deletes you from the pairs and print it 
+del row[11]
+print(row)
