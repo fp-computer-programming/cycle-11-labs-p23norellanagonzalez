56c1fa2d6327520dd6be7046135ec6875e5e895e
Nickolas. Orellana <p23norellanagonzalez@fairfieldprep.org>
2023-02-03 13:46:46 -0500

    almost done (missing codewars)

---
+++ Lab_11-4.py
@@ -0,0 +1,13 @@
+# Author NDO 02/02/23
+
+#Create a variable named weekdays and set it equal to a tuple that contains the days Monday-Friday as strings
+weekdays = ('Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday')
+
+#Create a variable named weekends and set it equal to a tuple that contains the days Saturday and Sunday as strings
+weekends = ('Saturday', 'Sunday')
+
+#Create a variable named days_of_the_week and set it equal to  that the concatenation of the two previous variables
+days_of_the_week = (weekdays + weekends)
+
+#Print the variable days_of_the_week and should display a tuple containing the days of the week in order 
+print(days_of_the_week)
---
+++ Lab_11-5.py
@@ -0,0 +1,16 @@
+# Author NDO 02/02/23 
+
+# create a list called day_date that contains the number day for the next 5 days staring with "2" for today
+day_date = [2,3,4,5,6]
+
+#using a tuple, assing the 5 letter from the list to a variable that represents each day using a single statement
+tuple(day_date)
+days_of_week = day_date
+(thu, fri, mon, tue, wed) = days_of_week
+
+#Print each of the variables to see what they equal
+print(thu)
+print(fri)
+print(mon)
+print(tue)
+print(wed)
